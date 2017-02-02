dabd61f04674b6ca5c1b6c1234bf8a5af662b3e8
Darin Estrin <darin.estrin@gmail.com>
2017-02-02 09:52:47 -0800

    i dont have chips but I have thin mints

--- main.js
+++ main.js
@@ -1,11 +1,66 @@
 $(document).ready(function () {
-    initializeGame();
+    //initializeGame();
 });
-function initializeGame() {
-    $("button").click(dropToken);
-}
+// function initializeGame() {
+//     $("button").click(dropToken);
+// }
+// var numRows = 5;
+// var numCols = 6;
+// var left = 1;
+// var right = 2;
+// var down = 3;
+// var diagRight = 1;
+// var diagLeft = 2;
+// var gameBoard = [
+//     [0],
+//     [0],
+//     [0],
+//     [0],
+//     [0]
+// ];
+// buildBoard();
+// function buildBoard() {
+//     var colChoice = Number(document.getElementById('chipColumn').value);
+//     console.log('Column  '+colChoice);
+//     for (var i = 0; i < numRows; i++) {
+//         for (var j = 0; j < numCols; j++) {
+//             gameBoard [i][j] = j;
+//         }
+//     }
+//     console.log(gameBoard);
+// }
+// function dropToken (row, column, direction, player) {
+// //drop Token code block
+//     testForWin(row, column, direction, player);
+// }
+// function testForWin (row, column, direction, player) {
+//     var winner = false;
+//     switch (direction) {
+//         case left:
+//             winner = ((gameBoard [row][column] = player) && (gameBoard [row][column-1]) && (gameBoard [row][column-2]));
+//             break;
+//         case right:
+//             winner = ((gameBoard [row][column] = player) && (gameBoard [row]+[column + 1]) && (gameBoard [row+2][column + 2]));
+//             break;
+//         case diagLeft:
+//             winner = ((gameBoard [row][column] = player) && (gameBoard [row-1][column - 1]) && (gameBoard [row-2][column - 2]));
+//             break;
+//         case diagRight:
+//             winner = ((gameBoard [row][column] = player) && (gameBoard [row]+[column + 1]) && (gameBoard [row+2][column + 2]));
+//             break;
+//         case down:
+//             winner = ((gameBoard [row][column] = player) && (gameBoard [row - 1][column]) && (gameBoard [row - 2][column]));
+//             break;
+//     }
+//     return winner;
+// }
+
+////////////////////////////////////////////////////////////
+// Darin's progress
+
+
 var numRows = 5;
-var numCols = 7;
+var numCols = 6;
 var left = 1;
 var right = 2;
 var down = 3;
@@ -18,10 +73,10 @@ var gameBoard = [
     [0],
     [0]
 ];
-buildBoard();
+
 function buildBoard() {
-    var colChoice = Number(document.getElementById('chipColumn').value);
-    console.log('Column  '+colChoice);
+
+    console.log('Column  '+columnChoice);
     for (var i = 0; i < numRows; i++) {
         for (var j = 0; j < numCols; j++) {
             gameBoard [i][j] = j;
@@ -55,10 +110,11 @@ function testForWin (row, column, direction, player) {
     return winner;
 }
 
-////////////////////////////////////////////////////////////
-// Darin's progress
 
 
+
+
+var turnOver = false;
 var currentPlayer = 1;
 var column1 = $(document).find('.column_1');
 var column2 = $(document).find('.column_2');
@@ -66,10 +122,13 @@ var column3 = $(document).find('.column_3');
 var column4 = $(document).find('.column_4');
 var column5 = $(document).find('.column_5');
 var column6 = $(document).find('.column_6');
+var columChoice;
 
 // select column to drop in
-$('div.zero').on('click', function(event){
-  console.log($(event));
+$(this).on('click', function(e){
+  console.log(e.target.className);
+  columnChoice = e.target;
+  handleCoinDrop(columnChoice);
 });
 
 function handleCoinDrop(column) {
@@ -105,26 +164,7 @@ function handleCoinDrop(column) {
       turnOver = true;
     }
   }
-  console.log(column);
-}
-
-function CheckForWin(){
-
-  // Need to check for vertical win by adding iterating down through column
-  // and checking for a match
-
-
-
-
-  // Check for horizontal win by comparing columns to both the left and right
-  // of the most recent chip dropped
-
-
-
-  // Check for diagnal win by using a combination for adding 1 to the row and 1
-  // to the columm, adding 1 to the row and substacting 1 from the column and vice versa.
-
-
+  //console.log(column);
 }
 
 /////////////////////////////////////////////////
