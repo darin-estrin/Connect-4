ec95a6e6291af8bdbe2ac10f72404ba3fd66c5b0
Darin Estrin <darin.estrin@gmail.com>
2017-02-02 13:04:52 -0800

    click handler progress

--- main.js
+++ main.js
@@ -1,119 +1,15 @@
 $(document).ready(function () {
     //initializeGame();
+    //findColumnClicked();
+    $('.column').click(function(e){
+      console.log(e);
+    });
 });
-// function initializeGame() {
-//     $("button").click(dropToken);
-// }
-// var numRows = 5;
-// var numCols = 6;
-// var left = 1;
-// var right = 2;
-// var down = 3;
-// var diagRight = 1;
-// var diagLeft = 2;
-// var gameBoard = [
-//     [0],
-//     [0],
-//     [0],
-//     [0],
-//     [0]
-// ];
-// buildBoard();
-// function buildBoard() {
-//     var colChoice = Number(document.getElementById('chipColumn').value);
-//     console.log('Column  '+colChoice);
-//     for (var i = 0; i < numRows; i++) {
-//         for (var j = 0; j < numCols; j++) {
-//             gameBoard [i][j] = j;
-//         }
-//     }
-//     console.log(gameBoard);
-// }
-// function dropToken (row, column, direction, player) {
-// //drop Token code block
-//     testForWin(row, column, direction, player);
-// }
-// function testForWin (row, column, direction, player) {
-//     var winner = false;
-//     switch (direction) {
-//         case left:
-//             winner = ((gameBoard [row][column] = player) && (gameBoard [row][column-1]) && (gameBoard [row][column-2]));
-//             break;
-//         case right:
-//             winner = ((gameBoard [row][column] = player) && (gameBoard [row]+[column + 1]) && (gameBoard [row+2][column + 2]));
-//             break;
-//         case diagLeft:
-//             winner = ((gameBoard [row][column] = player) && (gameBoard [row-1][column - 1]) && (gameBoard [row-2][column - 2]));
-//             break;
-//         case diagRight:
-//             winner = ((gameBoard [row][column] = player) && (gameBoard [row]+[column + 1]) && (gameBoard [row+2][column + 2]));
-//             break;
-//         case down:
-//             winner = ((gameBoard [row][column] = player) && (gameBoard [row - 1][column]) && (gameBoard [row - 2][column]));
-//             break;
-//     }
-//     return winner;
-// }
 
 ////////////////////////////////////////////////////////////
 // Darin's progress
 
-
-var numRows = 5;
-var numCols = 6;
-var left = 1;
-var right = 2;
-var down = 3;
-var diagRight = 1;
-var diagLeft = 2;
-var gameBoard = [
-    [0],
-    [0],
-    [0],
-    [0],
-    [0]
-];
-
-function buildBoard() {
-
-    console.log('Column  '+columnChoice);
-    for (var i = 0; i < numRows; i++) {
-        for (var j = 0; j < numCols; j++) {
-            gameBoard [i][j] = j;
-        }
-    }
-    console.log(gameBoard);
-}
-function dropToken (row, column, direction, player) {
-//drop Token code block
-    testForWin(row, column, direction, player);
-}
-function testForWin (row, column, direction, player) {
-    var winner = false;
-    switch (direction) {
-        case left:
-            winner = ((gameBoard [row][column] = player) && (gameBoard [row][column-1]) && (gameBoard [row][column-2]));
-            break;
-        case right:
-            winner = ((gameBoard [row][column] = player) && (gameBoard [row]+[column + 1]) && (gameBoard [row+2][column + 2]));
-            break;
-        case diagLeft:
-            winner = ((gameBoard [row][column] = player) && (gameBoard [row-1][column - 1]) && (gameBoard [row-2][column - 2]));
-            break;
-        case diagRight:
-            winner = ((gameBoard [row][column] = player) && (gameBoard [row]+[column + 1]) && (gameBoard [row+2][column + 2]));
-            break;
-        case down:
-            winner = ((gameBoard [row][column] = player) && (gameBoard [row - 1][column]) && (gameBoard [row - 2][column]));
-            break;
-    }
-    return winner;
-}
-
-
-
-
-
+var gameBoard = [[],[],[],[],[],[]];
 var turnOver = false;
 var currentPlayer = 1;
 var column1 = $(document).find('.column_1');
@@ -122,14 +18,10 @@ var column3 = $(document).find('.column_3');
 var column4 = $(document).find('.column_4');
 var column5 = $(document).find('.column_5');
 var column6 = $(document).find('.column_6');
-var columChoice;
+var columnChoice;
 
 // select column to drop in
-$(this).on('click', function(e){
-  console.log(e.target.className);
-  columnChoice = e.target;
-  handleCoinDrop(columnChoice);
-});
+
 
 function handleCoinDrop(column) {
   turnOver = false;
