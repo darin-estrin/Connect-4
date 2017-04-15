b5f0125b1e4a15a91b0ef11c5a498fa4184446d6
darin-estrin <darin.estrin@gmail.com>
2017-04-14 18:41:54 -0700

    formatted main.js

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-$(document).ready(function(){
+$(document)
+  .ready(function () {
     initGame();
     displayPlayer();
-    $('.reset_button').click(function() {
-        reset();
+    $('.reset_button').click(function () {
+      reset();
     });
-    // $('.reset_button').click(function(){
-    //   initG2();
-    // });
-});
+    // $('.reset_button').click(function(){   initG2(); });
+  });
 
 var gameBoard = [
         [0, 0, 0, 0, 0, 0],
@@ -25,45 +24,47 @@ var col6 = $('.column_6');
 var columnChosen;
 
 function initGame() {
-    $('#column_1').on('click', addPlayerCoin);
-    $('#column_2').on('click', addPlayerCoin);
-    $('#column_3').on('click', addPlayerCoin);
-    $('#column_4').on('click', addPlayerCoin);
-    $('#column_5').on('click', addPlayerCoin);
-    $('#column_6').on('click', addPlayerCoin);
+  $('#column_1').on('click', addPlayerCoin);
+  $('#column_2').on('click', addPlayerCoin);
+  $('#column_3').on('click', addPlayerCoin);
+  $('#column_4').on('click', addPlayerCoin);
+  $('#column_5').on('click', addPlayerCoin);
+  $('#column_6').on('click', addPlayerCoin);
 }
+
 function addPlayerCoin() {
   playerTurnOver = false;
-  if ($(this).hasClass('column_1')){
+  if ($(this).hasClass('column_1')) {
     column = col1;
     columnChosen = 0;
-  } else if ($(this).hasClass('column_2')){
+  } else if ($(this).hasClass('column_2')) {
     column = col2;
     columnChosen = 1;
-  }else if($(this).hasClass('column_3')){
+  } else if ($(this).hasClass('column_3')) {
     column = col3;
     columnChosen = 2;
-  }else if ($(this).hasClass('column_4')){
+  } else if ($(this).hasClass('column_4')) {
     column = col4;
     columnChosen = 3;
-  }else if ($(this).hasClass('column_5')){
+  } else if ($(this).hasClass('column_5')) {
     column = col5;
     columnChosen = 4;
   } else {
     column = col6;
     columnChosen = 5;
   }
+
   for (var i = column.length - 1; (!playerTurnOver && i > 0); i--) {
-    if ($(column[0]).hasClass('player1_token') || $(column[0]).hasClass('player2_token')){
+    if ($(column[0]).hasClass('player1_token') || $(column[0]).hasClass('player2_token')) {
       return;
     }
-    if (currentPlayer === 1  && !$(column[i]).hasClass('player1_token') && !$(column[i]).hasClass('player2_token')){
+    if (currentPlayer === 1 && !$(column[i]).hasClass('player1_token') && !$(column[i]).hasClass('player2_token')) {
       $(column[i]).addClass('player1_token');
       //saveData();
       playerTurnOver = true;
       var m = document.getElementById('mario_turn');
       m.play();
-    } else if(currentPlayer === 2  && !$(column[i]).hasClass('player1_token') && !$(column[i]).hasClass('player2_token')) {
+    } else if (currentPlayer === 2 && !$(column[i]).hasClass('player1_token') && !$(column[i]).hasClass('player2_token')) {
       $(column[i]).addClass('player2_token');
       //saveData();
       playerTurnOver = true;
@@ -71,6 +72,7 @@ function addPlayerCoin() {
       b.play();
     }
   }
+
   var row = gameBoard.indexOf(gameBoard[i]);
   if (currentPlayer === 1) {
     gameBoard[i][columnChosen] = 1;
@@ -82,127 +84,136 @@ function addPlayerCoin() {
   displayPlayer();
 }
 
-function checkForWin(currentPlayer, row, column){
+function checkForWin(currentPlayer, row, column) {
   console.log(row, column, currentPlayer)
   // check for horizontal left win
   var count = 1;
-  while(gameBoard[row][column-1] === currentPlayer && column - 1 >= 0){
+  while (gameBoard[row][column - 1] === currentPlayer && column - 1 >= 0) {
     count++;
     column--;
-    if(count === 4){
+    if (count === 4) {
       playerWon(currentPlayer);
     }
   }
-
-  // check horizont right win
   count = 1;
-  while(gameBoard[row][column+1] === currentPlayer && column + 1 <= 5){
+  // check horizont right win
+  while (gameBoard[row][column + 1] === currentPlayer && column + 1 <= 5) {
     count++;
-    column--;
-    if(count === 4){
+    column++;
+    if (count === 4) {
       playerWon(currentPlayer);
     }
   }
   count = 1;
 
   // check vertical win
-  while((row + 1) < 5 && currentPlayer === gameBoard[row+1][column]){
+  while ((row + 1) < 5 && currentPlayer === gameBoard[row + 1][column]) {
     count++;
     row++;
-    if(count === 4){
+    if (count === 4) {
       playerWon(currentPlayer);
     }
   }
   count = 1;
 
   // check diagonal-down left win
-  while((row + 1) < 5 && (column -1) >= 0 && currentPlayer === gameBoard[row+1][column-1]){
+  while ((row + 1) < 5 && (column - 1) >= 0 && currentPlayer === gameBoard[row + 1][column - 1]) {
     count++;
     row++;
     column--;
-    if(count === 4){
+    if (count === 4) {
       playerWon(currentPlayer);
     }
   }
   count = 1;
 
   // check diagonal-down right win
-  while((row + 1) < 5 && (column + 1) <= 5 && currentPlayer === gameBoard[row+1][column+1]){
+  while ((row + 1) < 5 && (column + 1) <= 5 && currentPlayer === gameBoard[row + 1][column + 1]) {
     count++;
     row++;
     column++;
-    if(count === 4){
+    if (count === 4) {
       playerWon(currentPlayer);
     }
   }
   count = 1;
 
   // check diagonal-up left win
-  while((row - 1) >= 0 && (column -1) >= 0 && currentPlayer === gameBoard[row-1][column-1]){
+  while ((row - 1) >= 0 && (column - 1) >= 0 && currentPlayer === gameBoard[row - 1][column - 1]) {
     count++;
     row--;
     column--;
-    if(count === 4){
+    if (count === 4) {
       playerWon(currentPlayer);
     }
   }
   count = 1;
 
   // check diagonal-up right win
-  while((row - 1) >= 0 && (column + 1) <= 5 && currentPlayer === gameBoard[row-1][column+1]){
+  while ((row - 1) >= 0 && (column + 1) <= 5 && currentPlayer === gameBoard[row - 1][column + 1]) {
     count++;
     row--;
     column++;
-    if(count === 4){
+    if (count === 4) {
       playerWon(currentPlayer);
     }
   }
-  
+
 }
-
-
 
 var playerTurnOver = false;
 var player1 = 1;
 var player2 = 2;
 var currentPlayer = player1;
 
-function displayPlayer (){
-  if(currentPlayer === 1) {
+function displayPlayer() {
+  if (currentPlayer === 1) {
     // changes players color to red if active and black if waiting
-    $('.player1').find('h2').addClass('active_player');
-    $('.player2').find('h2').removeClass('active_player');
+    $('.player1')
+      .find('h2')
+      .addClass('active_player');
+    $('.player2')
+      .find('h2')
+      .removeClass('active_player');
   } else {
     currentPlayer = 2;
-    $('.player2').find('h2').addClass('active_player');
-    $('.player1').find('h2').removeClass('active_player');
+    $('.player2')
+      .find('h2')
+      .addClass('active_player');
+    $('.player1')
+      .find('h2')
+      .removeClass('active_player');
   }
 }
 function changeActivePlayer(player) {
-  if (player === 1 ) {
-      currentPlayer = 2;
+  if (player === 1) {
+    currentPlayer = 2;
   } else if (player === 2) {
-      currentPlayer = 1;
+    currentPlayer = 1;
   }
   //replaceOldWithNew();
 }
-function playerWon(currentPlayer){
-    var mario = document.getElementById('mario_win');
-    var bowser = document.getElementById('bowser_win');
-    $('#game_body').html('<h1 class="player_won"></h1>');
-    if (player1 === currentPlayer){
-        $('#game_body').find('h1').text('Congratulations, you beat Bowser');
-        mario.play();
-    }else{
-        $('#game_body').find('h1').text('Congratulations, you beat Mario');
-        bowser.play();
+function playerWon(currentPlayer) {
+  var mario = document.getElementById('mario_win');
+  var bowser = document.getElementById('bowser_win');
+  $('#game_body').html('<h1 class="player_won"></h1>');
+  if (player1 === currentPlayer) {
+    $('#game_body')
+      .find('h1')
+      .text('Congratulations, you beat Bowser');
+    mario.play();
+  } else {
+    $('#game_body')
+      .find('h1')
+      .text('Congratulations, you beat Mario');
+    bowser.play();
 
-    }
+  }
 }
-function reset(){
-    $('.coin_slot').removeClass('player1_token');
-    $('.coin_slot').removeClass('player2_token');
-    var currentPlayer = player1;
+function reset() {
+  $('.coin_slot').removeClass('player1_token');
+  $('.coin_slot').removeClass('player2_token');
+  var currentPlayer = player1;
 }
 
 
